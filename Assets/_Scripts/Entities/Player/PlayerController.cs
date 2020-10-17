5f75ca65cf9c52b3239eb093f9b084480598c513
GonDie2709 <goncalves.die@gmail.com>
2020-10-17 05:51:43 -0300

    - Added PlayerController and states for idle, running and falling - Added Cinemachine

---
+++ Assets/_Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerState { Idle, Running, Falling }
+
+    [SerializeField] float movementSpeed = 1f;
+
+    Animator _animator;
+    CharacterController2D _characterController;
+
+    PlayerState _lastState;
+    PlayerState _currentState;
+    float _moveSpeed;
+    bool _hasJumped, _isJumping, _isSecondJumping;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController2D>();
+        _characterController.OnFallEvent.AddListener(() => SetPlayerState(PlayerState.Falling));
+        _characterController.OnLandEvent.AddListener(() => SetPlayerState(PlayerState.Idle));
+    }
+
+    void Update()
+    {
+        switch (_currentState)
+        {
+            case PlayerState.Idle:
+                IdleState();
+            break;
+            case PlayerState.Running:
+                MovingState();
+            break;
+            case PlayerState.Falling:
+                FallingState();
+            break;
+        }
+
+        _lastState = _currentState;
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_moveSpeed * Time.fixedDeltaTime, false, _hasJumped);
+        _hasJumped = false;
+    }
+
+    #region States
+    void IdleState()
+    {
+        _isJumping = false;
+        _isSecondJumping = false;
+
+        _moveSpeed = GetMovementSpeed();
+        if (Mathf.Abs(_moveSpeed) > 0f)
+            SetPlayerState(PlayerState.Running);
+
+        DoJump();
+    }
+
+    void MovingState()
+    {
+        _isJumping = false;
+        _isSecondJumping = false;
+
+        _moveSpeed = GetMovementSpeed();
+        if (Mathf.Abs(_moveSpeed) == 0f)
+            SetPlayerState(PlayerState.Idle);
+
+        DoJump();
+    }
+
+    void FallingState()
+    {
+        _isJumping = true;
+        _moveSpeed = GetMovementSpeed();
+
+        DoJump();
+    }
+    #endregion
+
+    float GetMovementSpeed()
+    {
+        return Input.GetAxisRaw("Horizontal") * movementSpeed;
+    }
+
+    void DoJump()
+    {
+        if (_isSecondJumping)
+            return;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!_isJumping)
+            {
+                _isJumping = true;
+                _hasJumped = true;
+            }
+            else
+            {
+                _isSecondJumping = true;
+                _hasJumped = true;
+            }
+        }
+    }
+
+    void SetPlayerState(PlayerState state)
+    {
+        if (_lastState == state)
+            return;
+
+        _currentState = state;
+        _animator.Play(state.ToString());
+    }
+}
